ad27d50016d2ac37ff6b0193ca1bb559233e777a
+My <kaypav@gmail.com>
2013-11-25 10:43:15 -0700

    add maths for snapping to grid

--- HoradricCube/Assets/Scripts/StoresContentsInAGrid.cs
+++ HoradricCube/Assets/Scripts/StoresContentsInAGrid.cs
@@ -27,17 +27,16 @@ public class StoresContentsInAGrid : MonoBehaviour
 
     public virtual bool TryToAddAt(Transform item, Vector2 at)
     {
-        Vector3 itemSize = item.collider.bounds.size;
+        Vector2 halfSize = item.collider.bounds.size / 2;
+        Vector2 snapTo = new Vector2(Mathf.Round(at.x - halfSize.x), Mathf.Round(at.y - halfSize.y)) + halfSize;
 
-        for (float y = transform.position.y + at.y + 0.5f - itemSize.y / 2; y < transform.position.y + at.y + itemSize.y / 2; y++)
+        for (float y = transform.position.y + snapTo.y + 0.5f - halfSize.y; y < transform.position.y + snapTo.y + halfSize.y; y++)
         {
-            for (float x = transform.position.x + at.x + 0.5f - itemSize.x / 2; x < transform.position.x + at.x + itemSize.x / 2; x++)
+            for (float x = transform.position.x + snapTo.x + 0.5f - halfSize.x; x < transform.position.x + snapTo.x + halfSize.x; x++)
             {
                 for (int i = 0; i < transform.childCount; i++)
                 {
                     Transform child = transform.GetChild(i);
-
-                    Debug.Log(child.collider.bounds + " :: " + new Vector3(x, y, transform.position.z));
 
                     if (child.collider.bounds.Contains(new Vector3(x, y, transform.position.z)))
                     {
@@ -48,7 +47,7 @@ public class StoresContentsInAGrid : MonoBehaviour
         }
 
         item.parent = transform;
-        item.localPosition = at;
+        item.localPosition = snapTo;
         return true;
     }
 }
